840be386a29e08b57e3fce8cf2faab499a6bacb3
UzNik <uznik.99@mail.ru>
2024-04-01 20:14:56 +0300

    Edited the templates and added routes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import uvicorn
 
 app = FastAPI(title="simple-store")
 
@@ -9,16 +10,18 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def home():
-    return "Hello world!"
 
 @app.get("/home", response_class=HTMLResponse)
-async def read_item(request: Request):
+async def home(request: Request):
     return templates.TemplateResponse(request=request, name="home.html")
 
+@app.get("/login", response_class=HTMLResponse)
+async def login(request: Request):
+    return templates.TemplateResponse(request=request, name="login_register.html")
  
-
+@app.get("/catalog", response_class=HTMLResponse)
+async def catalog(request: Request):
+    return templates.TemplateResponse(request=request, name="catalog.html")
 
 # if __name__ == "__main__":
 #     uvicorn.run(app, host="0.0.0.0", port=8000)
